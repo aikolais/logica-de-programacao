e5aa2a1724ee4be42d903aee20990b1fe87b0f7c
lais.dalpozur <lais.dalpozur@w3k.com.br>
2022-01-14 16:56:45 -0300

    ....

---
+++ loops1.js
@@ -0,0 +1,15 @@
+// let c = 1;
+
+// while(c <= 6) {
+//     console.log(`Executado em ${c}`)
+//     c++              // c = c + 1 
+// }
+
+// let c = 1;
+// do { 
+//     console.log(`Executado em ${c}`)
+//     c++
+// } while (c <= 6)
+
+
+
